01886e40a161d05c3973dede02aef051cd524a70
Alannah Kearney <alannah@ruleandmake.com>
2018-11-11 12:19:01 +0000

    Updated PropertyBag class to implement Iterator. Can use an instance of PropertyBag in, for example, foreach loops.

--- src/Lib/PropertyBag.php
+++ src/Lib/PropertyBag.php
@@ -2,9 +2,35 @@
 
 namespace pointybeard\PropertyBag\Lib;
 
-class PropertyBag {
+class PropertyBag implements \Iterator{
 
     protected $properties = [];
+    protected $position = 0;
+
+    protected function keyFromPosition($position) {
+        $index = array_keys($this->properties);
+        return isset($index[$position]) ? $index[$position] : -1;
+    }
+
+    public function rewind() {
+        $this->position = 0;
+    }
+
+    public function current() {
+        return $this->properties[$this->keyFromPosition($this->position)];
+    }
+
+    public function key() {
+        return $this->keyFromPosition($this->position);
+    }
+
+    public function next() {
+        ++$this->position;
+    }
+
+    public function valid() {
+        return isset($this->properties[$this->keyFromPosition($this->position)]);
+    }
 
     public function __get($name) {
         if(!isset($this->properties[$name])) {
